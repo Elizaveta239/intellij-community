517a3b027fe89011da00b57c8f904afc5b74c9d2
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2015-04-03 17:05:37 +0300

    Change color scheme in graph

--- python/src/com/jetbrains/python/debugger/threading/tool/graph/GraphSettings.java
+++ python/src/com/jetbrains/python/debugger/threading/tool/graph/GraphSettings.java
@@ -9,7 +9,7 @@ public class GraphSettings {
   public static int STROKE_WITH_LOCK = 4;
   public static int STROKE_BASIC = 4;
   public static boolean USE_STD_COLORS = true;
-  public static Color BASIC_COLOR = Color.GREEN;
-  public static Color LOCK_WAIT_COLOR = Color.ORANGE;
-  public static Color LOCK_OWNING_COLOR = Color.RED;
+  public static Color BASIC_COLOR = new Color(125, 125, 125);
+  public static Color LOCK_WAIT_COLOR = new Color(255, 179, 3);
+  public static Color LOCK_OWNING_COLOR = new Color(120, 255, 0);
 }
